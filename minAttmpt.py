db77e2b8cb0334167df11000d3e54f7d14b25270
qaswarh <qaswar@inabia.com>
2020-02-02 22:35:35 -0800

    Create minAttmpt.py

---
+++ minAttmpt.py
@@ -0,0 +1,22 @@
+""" Score points with min number of attempts to reach the target
+starting with points[0] each points[i] is an attempt irrespective of (points[i] - points[0]) != target
+problem get solved when points[i] - points[0] >= target """
+
+
+target = 11
+points = [5, 8, 10, 150, 180]
+
+
+def minAttmpt(thresh, score):
+    i = 1
+    count = 1
+    while i < len(points)-1:
+        if points[i] - points[0] >= thresh or points[i+1] - points[0] >= thresh:
+            return count + 1
+        else:
+            count += 1
+            i += 2
+    return len(points)
+
+
+print(minAttmpt(target, points))
