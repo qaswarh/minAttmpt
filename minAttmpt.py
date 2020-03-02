b42318dd03d4802be3db97c88f4a4c9758c6a2f4
Qaswar Hussain <47313728+qaswarh@users.noreply.github.com>
2020-03-01 21:14:16 -0800

    Update minAttmpt.py

--- minAttmpt.py
+++ minAttmpt.py
@@ -1,7 +1,7 @@
 """ Score points with min number of attempts to reach the target
 Starting with points[0], each points[i] is an attempt irrespective of (points[i] - points[0]) != target
-Problem get solved when points[i] - points[0] >= target """
-
+Problem get solved when points[i] - points[0] >= target 
+Here are target and points as an example, input to the code """
 
 target = 11
 points = [5, 8, 10, 150, 180]
